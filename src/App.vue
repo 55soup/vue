e5a382f746562759cfbd9d81c5d97865aaaa3c82
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-03-19 01:34:58 +0900

    init repo vue가보자~!

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4>xx 원룸</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>xx 원룸</h4>
+    <p>50 만원</p>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
