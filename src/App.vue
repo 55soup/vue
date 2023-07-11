0105c569a77d31554f7b12a971feb8e18c760779
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-07-12 01:17:11 +0900

    Add: oneroom data v-for, modal id

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="black-bg" v-if="isModalOn">
     <div class="white-bg">
-      <h4>상세페이지</h4>
-      <p>상세페이지 내용</p>
+      <h4>{{ onerooms[pressId].title }}</h4>
+      <p>상{{ onerooms[pressId].content }}</p>
+      <button @click="isModalOn=false">닫기</button>
     </div>
   </div>
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <div v-for="(product, i) in products" :key="i">
-    <img :src="imgUrl[i]" class="room-img"/>
-    <h4 @click="isModalOn=true">{{ product }}</h4>
-    <p>50</p>
-    <button @click="increase(i)">허위매물신고</button> <span>신고수 : {{ 신고수[i] }}</span>
+  <div v-for="(room, i) in onerooms" :key="i">
+    <img :src="room.image" class="room-img">
+    <h4 @click="() => {isModalOn=true; pressId=i;}">{{ room.title }}</h4>
+    <p>{{ room.price }}원</p>
   </div>
 </template>
 
 <script>
+import data from './assets/oneroom.js';
+
 export default {
   name: "App",
   data() {
     return {
       // data는 obj형식
+      pressId : 0,
       isModalOn: false,
       스타일: "color: blue",
       신고수 : [0,0,0], 
       menus: ['Home', 'Shop', 'About'],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      onerooms : data,
       imgUrl: [require('./assets/room0.jpg'), require('./assets/room1.jpg'), require('./assets/room2.jpg')],
     };
   },
