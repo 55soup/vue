56930817c2965772e974afd0c556b5a81172c723
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-07-22 23:33:16 +0900

    Add: 블로그글 추가. 데이터 바인딩 연습

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+    <div class="container-fluid">
+      <a class="navbar-brand">blog</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  <div class="container mt-4">
+    <h5>개발자 블로그</h5>
+    <p>made from vue</p>
   </div>
-</nav>
+  <List v-for="(content, i) in contents" :key="i" :content="content"/>
 </template>
 
 <script>
+import List from "./components/List"
+import blog from "./assets/blog"
 
 export default {
   name: 'App',
+  data(){
+    return {
+      contents: blog,
+    }
+  },
   components: {
+    List: List,
   }
 }
 </script>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h5>{{content.title}}</h5>
+    <p>{{content.date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props : {
+        content : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
