bf4177a2bfdee9cd783b05bf4a27795bb369332f
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-07-25 11:09:07 +0900

    vuestagram 프로젝트 생성, 레이아웃

---
+++ vuestagram/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+  </div>
+
+  <Container />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Container from "./components/Container";
+
+export default {
+  name: 'App',
+  components: {
+    Container : Container,
+  },
+}
+</script>
+
+<style>
+@import 'style.css';
+</style>
