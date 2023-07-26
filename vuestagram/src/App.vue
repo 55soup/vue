79d41dabd63cba981863f069c7b4d708487e3c22
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-07-27 00:59:45 +0900

    router대신 tab view

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -7,7 +7,7 @@
       <li>Next</li>
     </ul>
   </div>
-  <Container :post="post"/>
+  <Container :post="post" :step="step"/>
   <button @click="more">더보기</button>
 
   <div class="footer">
@@ -16,6 +16,7 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+  
 </template>
 
 <script>
@@ -27,7 +28,9 @@ export default {
   name: 'App',
   data() {
     return{
-      post : post,
+      post : post, // 게시물 데이터
+      clickCount : 0, //버튼 누른 횟수
+      step : 0, // tab view UI
     }
   },
   components: {
@@ -35,10 +38,14 @@ export default {
   },
   methods : {
     more(){
-      axios.get("https://codingapple1.github.io/vue/more1.json")
+      axios.get(`https://codingapple1.github.io/vue/more${this.clickCount}.json`)
       .then((result) => {
         console.log(result)
         this.post.push(result.data);
+        this.clickCount += 1;
+      })
+      .catch((err)=>{
+        console.log(err);
       })
     }
   }
